1ebb1daa3da73fa48650e04cca930f1bf863bfe1
Charles Engelke <charles.engelke@infotechfl.com>
2015-10-05 18:39:06 -0400

    Minor change

--- content/index.php
+++ content/index.php
@@ -56,7 +56,7 @@
       ?>
       
       <p>
-      <?php echo "This is instance <tt>{$instance_id}</tt>, {$article} <tt>{$instance_type}</tt> at <tt>{$local_ipv4}</tt>." ?>
+      <?php echo "This is updated instance <tt>{$instance_id}</tt>, {$article} <tt>{$instance_type}</tt> at <tt>{$local_ipv4}</tt>." ?>
       </p>
     </div>
   </body>
